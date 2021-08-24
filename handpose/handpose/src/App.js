685a9af7b9c7832d0a1cb45005f6e6f350347066
julian <juliandavid.alfonso@utp.edu.co>
2021-08-24 18:27:27 -0500

    aplicación ya carga la camara y dependencias instaladas

---
+++ handpose/handpose/src/App.js
@@ -0,0 +1,51 @@
+import React, {useRef} from 'react';
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam  from 'react-webcam';
+import './App.css';
+
+function App() {
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+        }}/>
+
+        <canvas
+        ref={canvasRef}
+        style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+
+        }}/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
