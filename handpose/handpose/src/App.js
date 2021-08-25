9709e4a737c93467a06aaf8dced04e6650c02147
julian <juliandavid.alfonso@utp.edu.co>
2021-08-25 09:35:00 -0500

    reconocimiento de señales completado

--- handpose/handpose/src/App.js
+++ handpose/handpose/src/App.js
@@ -4,11 +4,54 @@ import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam  from 'react-webcam';
 import './App.css';
+import { drawHand } from './utilities';
+
 
 function App() {
 
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+
+
+  const runHandpose = async () =>
+  {
+    const net = await handpose.load()
+    console.log('handpose model loaded')
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 100);
+    };
+  const detect = async (net) =>
+  {
+    //check data is acailable 
+
+    if (typeof webcamRef.current !== "undefined" &&
+    webcamRef.current !== null &&
+    webcamRef.current.video.readyState === 4
+  ) {
+    //get video properties
+    const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+    //set video width
+    webcamRef.current.video.width = videoWidth;
+    webcamRef.current.video.height = videoHeight;
+    //set canvas height and width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+    //make detections
+    const hand = await net.estimateHands(video);
+    console.log(hand);
+
+    //draw mesh
+     const ctx = canvasRef.current.getContext("2d");
+     drawHand(hand,ctx)
+  
+    }
+  };
+
+  runHandpose();
 
   return (
     <div className="App">
